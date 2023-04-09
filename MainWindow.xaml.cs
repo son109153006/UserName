e89444cc2fea1006a18b3baa118765cc14203cdc
son109153006 <kymi20011018@gmail.com>
2023-04-10 00:43:52 +0800

    程式完成

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace user
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_go_Click(object sender, RoutedEventArgs e)
+        {
+            if(username.Text == "abc" && password.Text == "123")
+            {
+                txtInto.Text = "輸入成功，請繼續使用";
+            }
+            else
+            {
+                txtInto.Text = "輸入失敗，請重新輸入";
+            }
+        }
+    }
+}
